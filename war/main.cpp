1c1b06d604b3ef23ab1c851a1a14567c72d19566
BigDumpus <42816130+BigDumpus@users.noreply.github.com>
2018-10-01 01:07:02 -0400

    Add files via upload

--- war/main.cpp
+++ war/main.cpp
@@ -1,13 +1,94 @@
 #include "card.hpp"
 #include <iostream>
+#include <algorithm>
 
 // its getting late. i think we are going to have a study/ do this in class day tommorow ill just do the rest then.
+// its super not compiling so i cant test the functions i made for the game
+//
+void game(std::deque <Card> , std::deque <Card> );
+void deal(std::deque <Card> , std::deque <Card>, std::deque <Card> );
 int main()
 {
-    /*make_deck(); // how do i make the deck.
-    std::cout << c0;*/
+    // how do i make the deck. maybe Deck deck; deck.make_deck; well the first part seems right i guess but im getting that deck is empty even
+    std::deque <Card> deck (52);
+    //make_deck();
+    //std::cout << c0;
+    std::deque <Card> P1 (26);
+    std::deque <Card> P2 (26);
+
+
+    // sort // actually i only really need shuffle
+
+    // shuffle
+    std::shuffle(deck);
+
+    // function that divides deck in two
+    deal(P1, P2, deck);
+
+    game(P1, P2);
+
    return 0;
 }
+
+
+    // function that divides deck in two
+    void deal(std::deque <Card> x, std::deque <Card> y, std::deque <Card> deck)
+    {
+        while(!deck.empty())
+        {
+            x.push_front(deck.pop_front()); // puts top card from deck in x
+            y.push_front(deck.pop_front());
+        }
+    }
+
+
+    //function that puts two cards into a deque and compares them
+    // if they are == then ad two more cards to the deque
+    // if > then the cards go to p1 if < then cards go to p2
+    // include counter
+    void game(std::deque <Card> x, std::deque <Card> y)
+    {
+        int i =0;
+        std::deque <Card> hold (2); // creates deck to hold cards in play
+        while (!x.empty() && !y.empty())
+        {
+            hold.push_front(x.pop_front()); //takes card from top of x and puts in front of hold
+            hold.push_back(y.pop_front());  // take card from top of y and puts in back of hold
+            if((hold.front()).getRank() >( hold.(back())).getRank()) // card rank comparison
+            {
+                while (!hold.empty())
+                    x.push_back(hold.pop_front());
+            }
+            else if((hold.front()).getRank() < ( hold.(back())).getRank()) // if the high card is in front then x gets all the cards if high card is in back then y gets all cards. if neither card is high the loop loops
+            {
+                while (!hold.empty())
+                    y.push_back(hold.pop_front());
+            }
+            i++; // counter increments
+
+
+            /*else
+            {
+                //game(x, y);
+                wait do i acually need to do anything. it will just loop and put more cards on the end until either deck is empty // i think i need to overload the > and <
+            unless
+            }*/
+
+        }
+        if (x.empty())
+                std::cout<< "P2 won in "<< i << " turns";
+        else if (y.empty())
+                std::cout<< "P1 won in "<< i << " turns";
+    }
+
+    // function to check if either deck is empty and declared winner and number of turns.
+
+
+
+
+
+
+
 /* to do
 make card class:
     he already has a lot in the git hub but you still need to make the get_() functions and some where you need to overload > < = and cout which i dont remeber how to do
