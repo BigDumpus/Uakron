b2f1a51372e3e39f9d1ea4882e531e8df6c5934f
BigDumpus <42816130+BigDumpus@users.noreply.github.com>
2019-05-02 23:19:15 -0400

    Add files via upload

---
+++ desperate/desperate/projectlast.py
@@ -0,0 +1,47 @@
+# i think i might be in over my head in a very bad way. lets start easy
+print("hello world")
+# okay now do the project
+
+inger=2 #my performance in this course will certainly inger my GPA
+
+words="yowza im in big trouble"
+
+type(inger)
+type(words) #wow is this type checking
+#wow this probably isnt what he meant
+
+# Implement universally quantified types, type abstraction,
+# and type application. Implement type application during type
+# checking, so that the evaluated AST does not contain any type
+# abstractions or type applications.
+
+# okay i hope bad rambling py files that dont do anything make him not feel sad
+# because he is going away forever and i didnt make a reference to
+# escape from new york once despite my eyepatch
+
+# universally quantified types: somthing somthing haskell
+# the V thing that means for all
+#for all int have type int
+
+# type abstraction: type operation, value. abstract not concreate remove complexity
+# so its what python does? i dont know if not needing to define types means
+# type abstraction is occuring.
+
+# type application: apply a type.
+
+# so its going to be like the type for the language will be abstracted and the
+# type checking happens behind the scences(geez) for the user. so maybe like python
+
+# okay now i just need to grow an AST that does not contain abstrations or applications
+# of the type. also do the thing before that
+
+# Implement type application during type checking
+# AST contains no abstration or type applications
+
+#why did i do this to myself
+
+# def whattype(object)
+# if type(object) is int:
+# return int
+# if type(object) is bool:
+#return bool
