e8e6859ce96c6676c3f1051c84a239089c79c240
BigDumpus <42816130+BigDumpus@users.noreply.github.com>
2019-05-02 22:56:00 -0400

    Add files via upload

---
+++ desperate/projectlast.py
@@ -0,0 +1,37 @@
+# i think i might be in over my head in a very bad way. lets start easy
+print("hello world")
+# okay now do the project
+
+inger=2 #my performance in this course will certainly inger my GPA
+
+words="yowza im in big trouble" #in litle akron
+
+type(inger)
+type(words) #wow is this type checking
+#wow this probably isnt what he meant
+
+# Implement universally quantified types, type abstraction,
+# and type application. Implement type application during type
+# checking, so that the evaluated AST does not contain any type
+# abstractions or type applications.
+
+# okay i hope bad rambling py files that dont do anything make him not feel sad
+# because he is going away forever and i didnt make a reference to
+# escape from new york once dispite my eyepatch
+
+# universally quantified types: somthing somthing haskell somthing somthing
+# the V thing that means for all
+
+# type abstraction: type operation, value. abstract not concreate remove complexity
+# so its what python does? i dont know if not needing to define types means
+# type abstraction is occuring.
+
+# type application: apply a type. was it the type or the object or the chicken and the egg
+
+# so its going to be like the type for the language will be abstracted and the
+# type checking happens behind the scences(geez) for the user. so maybe like python
+
+#okay now i just need to grow an AST that does not contain abstrations or applications
+# of the type. also do the thing before that
+
+# Implement type application during type checking
